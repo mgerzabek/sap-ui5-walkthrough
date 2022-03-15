b5ef221647ce90b060aa1f51c70121ac990569e7
Michael Gerzabek <michael.gerzabek@gmail.com>
2022-03-15 18:21:17 +0100

    Inhalte bis Step 33

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+  "use strict";
+
+  return Controller.extend("sap.ui.demo.wt.controller.App", {
+    
+  });
+});
---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,33 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/routing/History"
+], function (Controller, History) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.wt.controller.Detail", {
+		
+    onInit: function () {
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+		},
+
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
+		},
+
+		_onObjectMatched: function (oEvent) {
+			this.getView().bindElement({
+				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+				model: "invoice"
+			});
+		}
+	});
+});
